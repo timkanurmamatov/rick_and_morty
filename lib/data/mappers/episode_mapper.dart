4c46936606e2b8396f64d3024c95d661a08244fc
Temirlan Nurmamatov <tima@actimi.com>
2025-07-17 18:49:17 +0600

    добавил картинку на экран деталей персонажа (BackdropFilter)

--- lib/data/mappers/episode_mapper.dart
+++ lib/data/mappers/episode_mapper.dart
@@ -7,10 +7,58 @@ class EpisodeMapper {
     return EpisodeEntity(
       id: dto.id,
       name: dto.name,
-      airDate: DateTime.parse(dto.airDate),
+      airDate: DateTime.tryParse(dto.airDate) ?? _parseDateTime(dto.airDate),
       characterIdList: dto.characters
           .map((e) => Utils.getIdFromUrl(e))
           .toList(),
     );
+  }
+
+  static DateTime _parseDateTime(String str){
+    // December 2, 2013
+    str = str.replaceAll(",", "");
+    // December 2 2013
+    List<String> splitted = str.split(" ");
+    // ["December", "2", "2013"]
+    late int month;
+    switch(splitted[0].toLowerCase()){
+      case "january":
+        month = 1;
+        break;
+      case "february":
+        month = 2;
+        break;
+      case "march":
+        month = 3;
+        break;
+      case "april":
+        month = 4;
+        break;
+      case "may":
+        month = 5;
+        break;
+      case "june":
+        month = 6;
+        break;
+      case "july":
+        month = 7;
+        break;
+      case "august":
+        month = 8;
+        break;
+      case "september":
+        month = 9;
+        break;
+      case "october":
+        month = 10;
+        break;
+      case "november":
+        month = 11;
+        break;
+      default:
+        month = 12;
+        break;
+    }
+    return DateTime(int.parse(splitted[2]), month, int.parse(splitted[1]));
   }
 }
--- lib/presentation/characters/ui/screens/character_details_screen.dart
+++ lib/presentation/characters/ui/screens/character_details_screen.dart
@@ -1,5 +1,8 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rick_and_morty/constants/image_paths.dart';
 import 'package:rick_and_morty/presentation/characters/ui/bloc/character_details_cubit/cubit/character_details_cubit.dart';
 
 class CharacterDetailsScreen extends StatefulWidget {
@@ -40,7 +43,32 @@ class _CharacterDetailsScreenState extends State<CharacterDetailsScreen> {
       body: BlocConsumer<CharacterDetailsCubit, CharacterDetailsState>(
         bloc: _cubit,
         listener: (context, state) {
-          // TODO: implement listener
+          if (state is CharacterDetailsError) {
+            showDialog(
+              context: context,
+              builder: (context) {
+                return AlertDialog(
+                  actions: [
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: Text("Понял"),
+                    ),
+                  ],
+                  icon: Icon(
+                    Icons.error,
+                    size: 50,
+                    color: Theme.of(context).colorScheme.error,
+                  ),
+                  content: Text(
+                    state.message,
+                    textAlign: TextAlign.center,
+                  ),
+                );
+              },
+            );
+          }
         },
         builder: (context, state) {
           if (state is CharacterDetailsLoading) {
@@ -48,38 +76,63 @@ class _CharacterDetailsScreenState extends State<CharacterDetailsScreen> {
               child: CircularProgressIndicator(),
             );
           }
-          return Column(
-            children: [
-              SizedBox(
-                height: 296,
-                child: Stack(
-                  alignment: Alignment.bottomCenter,
-                  children: [
-                    Column(
-                      children: [
-                        Container(
-                          height: 218,
-                          color: Colors.red,
-                        ),
-                      ],
-                    ),
-                    Container(
-                      height: 162,
-                      width: 162,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(100),
-                        border: Border.all(
-                          color: Theme.of(context).colorScheme.surface,
-                          width: 8,
-                        ),
-                        color: Colors.amber,
+          if (state is CharacterDetailsSuccess) {
+            Widget image = FadeInImage.assetNetwork(
+              fit: BoxFit.cover,
+              placeholder: ImagePaths.placeholder,
+              image: state.character.image,
+            );
+            return Column(
+              children: [
+                SizedBox(
+                  height: 296,
+                  child: Stack(
+                    alignment: Alignment.bottomCenter,
+                    children: [
+                      Column(
+                        children: [
+                          SizedBox(
+                            height: 218,
+                            width: double.infinity,
+                            child: Stack(
+                              fit: StackFit.expand,
+                              children: [
+                                image,
+                                Positioned.fill(
+                                  child: BackdropFilter(
+                                    filter: ImageFilter.blur(
+                                      sigmaX: 5,
+                                      sigmaY: 5,
+                                    ),
+                                    child: Container(
+                                      color: Colors.transparent,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
                       ),
-                    ),
-                  ],
+                      Container(
+                        height: 162,
+                        width: 162,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(100),
+                          border: Border.all(
+                            color: Theme.of(context).colorScheme.surface,
+                            width: 8,
+                          ),
+                        ),
+                        child: ClipOval(child: image),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            ],
-          );
+              ],
+            );
+          }
+          return SizedBox.shrink();
         },
       ),
     );
