96266613d4c638c8ff86b3961784d63062463617
Temirlan Nurmamatov <tima@actimi.com>
2025-07-18 19:07:03 +0600

    добавил примеры сортировки списка

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,17 @@ import 'package:rick_and_morty/home_screen.dart';
 import 'package:rick_and_morty/presentation/characters/ui/bloc/character_bloc/character_bloc.dart';
 
 void main() {
+
+  // Примеры сортировки элементов в списке
+  // List<String> a = ["a", "d", "c", "ax", "qwe"];
+  // List<int> b = [1,4,3452,45623,1,234,654];
+
+  // a.sort((a, b) => b.compareTo(a));
+  // b.sort((a, b) => b.compareTo(a));
+
+  // print(b);
+
+
   runApp(
     MultiBlocProvider(
       providers: [
